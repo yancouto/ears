b8b405364137ed59e65ad9aa13ef9a92a89a2509
Jan Niklas Hasse <jhasse@bixense.com>
2016-01-20 22:56:20 +0100

    Also play wav file in basic example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,14 +1,19 @@
 extern crate ears;
 
-use ears::*;
+use ears::{Sound, AudioController};
 
-fn main() {
+fn play_file(file: &str) {
     // Create a new Sound.
-    let mut snd = Sound::new("examples/assets/test.ogg").unwrap();
+    let mut snd = Sound::new(file).unwrap();
 
     // Play the Sound
     snd.play();
 
     // Wait until the end of the sound
     while snd.is_playing() {}
+}
+
+fn main() {
+    play_file("res/shot.wav");
+    play_file("res/explosion.ogg");
 }
