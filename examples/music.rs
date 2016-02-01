f721e9d081216bee70824e4dfa0d7a27ca189259
Jan Niklas Hasse <jhasse@gmail.com>
2016-02-01 09:22:07 +0100

    Sleep in Music example

--- examples/music.rs
+++ examples/music.rs
@@ -1,9 +1,13 @@
 extern crate ears;
 
 use ears::{Music, AudioController};
+use std::time::Duration;
+use std::thread::sleep;
 
 fn main() {
     let mut music = Music::new("res/music.ogg").unwrap();
     music.play();
-    while music.is_playing() {}
+    while music.is_playing() {
+        sleep(Duration::from_millis(1000));
+    }
 }
