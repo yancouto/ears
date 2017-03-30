676f597e0cb00f0722e9eb7023c1de31ff954c14
Daan Rijks <daanrijks@gmail.com>
2017-03-30 15:22:26 +0200

    Fix linking on Windows
    I had some problems with the linker not finding libsndfile and OpenAL
    when building on Windows using the GNU toolchain. This commit modifies
    `build.rs` so that the linker looks in MSYS's libs directory, even when
    not running from the MSYS shell.

--- build.rs
+++ build.rs
@@ -4,5 +4,15 @@ fn main() {
     println!("cargo:rustc-link-search=native=/usr/local/lib");
 }
 
-#[cfg(not(unix))]
+#[cfg(all(windows, target_arch = "x86"))]
+fn main () {
+    println!("cargo:rustc-link-search=native=C:\\msys32\\mingw64\\lib");
+}
+
+#[cfg(all(windows, target_arch = "x86_64"))]
+fn main () {
+    println!("cargo:rustc-link-search=native=C:\\msys64\\mingw64\\lib");
+}
+
+#[cfg(all(not(windows), not(unix)))]
 fn main() {}
